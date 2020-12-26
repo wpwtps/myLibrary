e8c50ef8b311ee6d44aac9192dd085d8d4988108
wpwtps <Wipawas.T@hotmail.com>
2020-12-27 03:16:30 +0700

    show css, create data table, call var

--- my_library/settings.py
+++ my_library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book_management',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/book_management/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'book_management/static/',
+]
